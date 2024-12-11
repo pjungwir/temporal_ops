cbe35af7118cac1727224f41926264705a710d70
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-12-10 21:11:48 -0800

    Add a test with duplicates for UNION

--- sql/union.sql
+++ sql/union.sql
@@ -6,3 +6,12 @@ FROM  (
 ) x
 GROUP BY id
 ORDER BY id, valid_at;
+
+SELECT  id, UNNEST(range_agg(valid_at)) AS valid_at
+FROM  (
+  SELECT * FROM a2 as a
+  UNION
+  SELECT * FROM b2 as b
+) x
+GROUP BY id
+ORDER BY id, valid_at;
